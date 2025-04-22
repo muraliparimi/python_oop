808e63fab1dccb71a0393b573aea55d1e35e73bd
muraliparimi <mkrishna.parimi@gmail.com>
2025-04-22 10:59:04 -0500

    more slinding window problems

--- sliding_window_problems.py
+++ sliding_window_problems.py
@@ -12,7 +12,10 @@ def closeDuplicates(nums, k):
         window.add(nums[R])
     return False
 
-class Solution:
+
+# number of subarrays of size k with average greater than given threshold.
+
+class numOfSubarrays:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
         ans = 0
         target_sum = threshold * k
@@ -33,6 +36,40 @@ class Solution:
             if window_sum >= target_sum:
                 ans +=1
         return ans
+    
+
+# subarray with same number repetition in a given array. find its length.
+class LengthySubarray:
+    def lengthySubarray(self, arr: List[int]) -> int:
+        ans = 1,
+        max_len = 1
+        L = 0
+        R = L + 1
+        while L <= R < len(arr):
+            if arr[L] == arr[R]:  
+                ans += 1
+                max_len = max(ans, max_len)
+                R += 1
+            else:
+                L=R
+                ans = 1
+                R +=1
+        return max_len
+    
+    def longestsubarray(self, arr: List[int]) -> int:
+        length = 0
+        L = 0
+        for R in range(len(arr)):
+            if arr[L] != arr[R]:
+                L = R
+            length = max(length, R - L + 1)
+        return length
+
+# Leetcode 
+
+
 
 if __name__ == '__main__':
-    print(closeDuplicates([1,2,3,2,3,3], 3))
+    x = LengthySubarray()
+    print(x.lengthySubarray([1,2,2,3,3,3]))
+    print(x.longestsubarray([1,2,2,3,3,3]))
